e1402027db05f876ebcb0b7b68c7ca268f39c08a
dipak chavan <chavandipak0606@gmail.com>
2026-03-16 12:44:57 +0530

    logout bc and ft

--- Backend/controller/commCont.js
+++ Backend/controller/commCont.js
@@ -76,3 +76,12 @@ module.exports.checkToken = async (req, res) => {
     })
 
 }
+
+
+module.exports.logout = async (req, res) => {
+    const token = req.cookies.token
+    if (!token) return res.status(401).json({ message: 'No token provided' });
+
+    res.clearCookie('token');
+    res.status(200).json({ message: 'Logged out successfully' });
+}
